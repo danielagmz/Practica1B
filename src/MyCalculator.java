151b095b372bfcce7ed796b4f7cf2c1f91fac7a1
danielagmz <161260538+danielagmz@users.noreply.github.com>
2024-03-25 18:37:36 +0100

    Implementado metodo divisio para el ejercicio 2

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -1,5 +1,6 @@
 import megaLibreria.utilities;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MyCalculator {
@@ -24,11 +25,19 @@ public class MyCalculator {
                         System.out.print("Introdueix la base i l'exponent): ");
                         n= scan.nextInt();
                         p= scan.nextInt();
-                        scan.nextLine();
                         longPower(n,p);
-
+                        scan.nextLine();
                         break;
                     case 2:
+                        int x;
+                        int y;
+
+                        System.out.print("Introdueix el numerador: ");
+                        x= scan.nextInt();
+                        System.out.print("Introdueix el denominador: ");
+                        y= scan.nextInt();
+                        scan.nextLine();
+                        divisio(x,y);
 
                         break;
                     case 3:
@@ -41,8 +50,13 @@ public class MyCalculator {
                         System.out.println("ATENCIÓ!!! \nHa de ser un valor entre 0 i 3");
                 }
 
-            } catch (Exception e) {
+            } catch (NumberFormatException e) {
                 System.out.println(e.getMessage());
+            }catch (InputMismatchException e2){
+                System.out.println(e2);
+                scan.nextLine();
+            }catch (ArithmeticException e3){
+                System.out.println(e3);
             }
             System.out.println();
             Thread.sleep(700);
@@ -62,6 +76,9 @@ public class MyCalculator {
             }
         }
 
+    }
+    public static void divisio(int x,int y){
+        System.out.println(x/y);
     }
 }
 
